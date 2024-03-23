e94c325692bfb9579a4d226d82824c54e6dbf0b0
Md Faisal Ahamed <faisalah331@gmail.com>
2024-03-23 23:54:50 +0600

    Forget Password Page

--- lib/controllers/forget_password_controller.dart
+++ lib/controllers/forget_password_controller.dart
@@ -8,71 +8,70 @@ import 'package:http/http.dart' as http;
 import '../screens/user_auth/OTP_verification/forget_pass_otp_verification.dart';
 
 class ForgetPassController extends GetxController {
-  GlobalKey<FormState> passKey = GlobalKey<FormState>();
   GlobalKey<FormState> key = GlobalKey<FormState>();
-   TextEditingController numberController = TextEditingController();
-   TextEditingController newPassword = TextEditingController();
-   TextEditingController confirmPassword = TextEditingController();
   GlobalKey<FormState> otpKey = GlobalKey<FormState>();
+  GlobalKey<FormState> passKey = GlobalKey<FormState>();
+
+  TextEditingController numberController = TextEditingController();
   TextEditingController otpController = TextEditingController();
+  TextEditingController newPassword = TextEditingController();
+  TextEditingController confirmPassword = TextEditingController();
 
 
-   numberValidate() async{
-     print("object");
-     String appUrl = "https://starsoftjpn.xyz/api/v1/forgot-password-by-phone";
-     if(key.currentState!.validate()){
-       print("object");
-       var res = await http.post(Uri.parse(appUrl),body: {
-         "phone" : numberController.text
-       });
-       print("object");
+  numberValidate() async {
+    print("object");
+    String appUrl = "https://starsoftjpn.xyz/api/v1/forgot-password-by-phone";
+    if (key.currentState!.validate()) {
+      print("object");
+      var res = await http
+          .post(Uri.parse(appUrl), body: {"phone": numberController.text});
+      print("object");
 
-       print(res.statusCode);
-       print(res.body);
+      print(res.statusCode);
+      print(res.body);
 
-       if(res.statusCode == 200){
-         Get.to(ForgetPassOtpVerification());
-       }
-     }
-   }
+      if (res.statusCode == 200) {
+        Get.to(ForgetPassOtpVerification());
+      }
+    }
+  }
 
-
-  otpValidate() async{
+  otpValidate() async {
     print("object");
     String appUrl = "https://starsoftjpn.xyz/api/v1/forgot-password-otp-check";
-    if(key.currentState!.validate()){
+    if (key.currentState!.validate()) {
       print("object");
-      var res = await http.post(Uri.parse(appUrl),body: {
-        "phone" : numberController.text,
-        "otp" : otpController.text,
+      var res = await http.post(Uri.parse(appUrl), body: {
+        "phone": numberController.text,
+        "otp": otpController.text,
       });
       print("object");
 
       print(res.statusCode);
       print(res.body);
 
-      if(res.statusCode == 200){
+      if (res.statusCode == 200) {
         Get.to(ForgetPasswordPage());
       }
     }
   }
 
-  forgetValidation() async{
+  forgetValidation() async {
     print("object");
     String appUrl = "https://starsoftjpn.xyz/api/v1/forgot-password-update";
-    if(key.currentState!.validate()){
+    if (key.currentState!.validate()) {
       print("object");
-      var res = await http.post(Uri.parse(appUrl),body: {
-        "phone" : numberController.text,
-        "password" : newPassword.text,
-        "confirm_password" : confirmPassword.text,
+      var res = await http.post(Uri.parse(appUrl), body: {
+        "phone": numberController.text,
+        "password": newPassword.text,
+        "confirm_password": confirmPassword.text,
       });
       print("object");
 
       print(res.statusCode);
       print(res.body);
 
-      if(res.statusCode == 200){
+      if (res.statusCode == 200) {
         Get.to(const LoginScreen());
       }
     }
